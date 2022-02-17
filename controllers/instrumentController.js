6697d119bdbad78a58109a598d3b3aeb44d2b12a
aresous <aresous@gmail.com>
2022-02-17 02:08:45 -0500

    error display below form

--- controllers/instrumentController.js
+++ controllers/instrumentController.js
@@ -3,7 +3,7 @@ const Brand = require('../models/brand')
 const Type = require('../models/type')
 const async = require('async');
 const { body, validationResult } = require('express-validator')
-
+const  currency  = require('currency.js');
 exports.index = function(req, res){
     async.parallel({
         instrument_count: function(cb){
@@ -37,7 +37,7 @@ exports.instrument_details = function(req,res){
     .populate('brand')
     .exec(function(err,results){
         if (err) { return next(err); }
-        res.render('instrument_details', {title:results.name, results})
+        res.render('instrument_details', {title:results.name, price: currency(results.price), results})
     })
 }
 exports.instrument_create_get = function(req,res){
@@ -53,13 +53,7 @@ exports.instrument_create_get = function(req,res){
     })
 }
 
-    // body('name').trim().isLength({ min: 1 }).escape().withMessage('Name must be specified.')
-    // .isAlphanumeric().withMessage('Name has non-alphanumeric characters.'),
-    // body('description').trim().isLength({ min: 1 }).escape().withMessage('Description must be specified.')
-    // .isAlphanumeric().withMessage('Description has non-alphanumeric characters.'),
-    // body('brand').trim().isLength({ min: 1 }).escape().withMessage('Description must be specified.')
-    // .isAlphanumeric().withMessage('Description has non-alphanumeric characters.'),
-    // body('price').isFloat({max: 2000000}).withMessage('Price is too high.'),
+
     // this goes in the post request below if i can get it to work
 
 exports.instrument_create_post = [
@@ -68,11 +62,14 @@ exports.instrument_create_post = [
         next();
     },
 
-    body('name','Empty Name'),
-    body('description','Empty Description'),
-    body('brand','Empty Brand'),
+    body('name').trim().isLength({ min: 1 }).escape().withMessage('Name must be specified.')
+    .isAlphanumeric().withMessage('Name has non-alphanumeric characters.'),
+    body('description').trim().isLength({ min: 1 }).escape().withMessage('Description must be specified.')
+    .isAlphanumeric().withMessage('Description has non-alphanumeric characters.'),
+    body('brand').trim().isLength({ min: 1 }).escape().withMessage('Description must be specified.')
+    .isAlphanumeric().withMessage('Description has non-alphanumeric characters.'),
+    body('price').isFloat({max: 2000000}).withMessage('Price is too high.'),
     body('type', 'Empty Type'),
-    body('price','Empty Price'),
     (req,res,next)=>{
         const errors = validationResult(req);
         const instrument = new Instrument(
@@ -85,11 +82,21 @@ exports.instrument_create_post = [
             }
         )
         if(errors.isEmpty()){
-            console.log('y1')
             instrument.save(function(err){
                 if(err){return next(err)}
                 res.redirect(instrument.url)
             })
-        } else {console.log(errors+'=======')}
+        } else {
+            async.parallel({
+                types: function(cb){
+                    Type.find({},cb)
+                },
+                brands: function(cb){
+                    Brand.find({},cb)
+                }
+            }, function(err,results){
+                res.render('instrument_create', {types:results.types, brands:results.brands, title: 'Create Instrument', error:errors, instrument:instrument})
+            })
+        }
     }
 ]
